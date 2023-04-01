edb0b334ce823306035da33f163763db977839e8
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-04-01 15:00:23 -0300

    Busca linear funcionando

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] a = {3,2,8,9,11,23};
+System.Console.Write(LinearSearch(a, 3));
+
+
+int LinearSearch(int[] array, int k)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == k)
+            return i;
+    }
+    return -1;
+}
